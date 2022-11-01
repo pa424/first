55ec116031909de4223a6ede6e8ca85c2bad2a4f
pa424 <akashkumarsrm16@gmail.com>
2022-11-01 13:35:06 +0530

    adding file

---
+++ day1/Helloworld.java
@@ -0,0 +1,18 @@
+
+
+public class Helloworld{
+
+
+	public static void main(String args[]){
+		while(true){
+			System.out.println("Sushmita");
+			try{
+				Thread.sleep(60000);
+				}catch(Exception ex){
+					System.out.println(ex);
+				}
+			}
+		}
+	}
+
+
